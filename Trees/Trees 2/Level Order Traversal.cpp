0865d4ab11ac04c822607bf1ce4999ae8a514bec
Divya Musahib <divyamusahib@gmail.com>
2025-03-11 23:44:15 +0530

    added tree problems

---
+++ Trees/Trees 2/Level Order Traversal.cpp
@@ -0,0 +1,53 @@
+// https://www.geeksforgeeks.org/problems/level-order-traversal/1
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    // Constructor
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+class Solution {
+    public:
+      vector<vector<int>> levelOrder(Node *root) {
+          vector<vector<int>> ans; 
+          if (root == nullptr) {
+              return ans; 
+          }
+          
+          queue<Node*> q; 
+          q.push(root); 
+  
+          while (!q.empty()) {
+              int size = q.size();
+              vector<int> level; 
+              for (int i = 0; i < size; i++) {
+                  Node* node = q.front(); 
+                  q.pop(); 
+                  level.push_back(node->data); 
+  
+                  if (node->left != nullptr) {
+                      q.push(node->left);
+                  }
+                  if (node->right != nullptr) {
+                      q.push(node->right);
+                  }
+              }
+              ans.push_back(level); 
+          }
+          return ans; 
+      }
+};
+
+int main() {
+
+}
